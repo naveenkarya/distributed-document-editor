fa8106f6487fef824a357bedfc4f4e447ccbc5e6
navee <naveenkkarya@gmail.com>
2022-05-24 20:47:32 -0700

    enableAsync

--- src/main/java/coen317/project/documenteditor/DocumentEditorApplication.java
+++ src/main/java/coen317/project/documenteditor/DocumentEditorApplication.java
@@ -2,10 +2,12 @@ package coen317.project.documenteditor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class DocumentEditorApplication {
 	public static int leader = 1;
 	public static void main(String[] args) {
