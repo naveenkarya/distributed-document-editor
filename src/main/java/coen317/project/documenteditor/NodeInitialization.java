07763974e46cf702be482001d2ebdf87180cd7f0
Naveen Kumar <naveenkarya@github.com>
2022-05-30 19:14:07 -0700

    add map for new node

--- src/main/java/coen317/project/documenteditor/NodeInitialization.java
+++ src/main/java/coen317/project/documenteditor/NodeInitialization.java
@@ -4,10 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -23,13 +30,28 @@ public class NodeInitialization {
 
     @EventListener(ApplicationReadyEvent.class)
     public void initializeNode() {
-        if (nodesInfo.isLeader()) {
-            String url = UriComponentsBuilder.newInstance()
-                    .scheme("http").host(nodesInfo.getLoadBalancer())
-                    .path(LB_ADD_NODE_PATH).buildAndExpand(nodesInfo.getSelfPort()).toUriString();
-            ResponseEntity<Integer> selfNode = restTemplate.getForEntity(url, Integer.class);
-            nodesInfo.setSelf(selfNode.getBody());
-        }
+        updateExistingNodes();
+        addSelf();
+    }
+
+    private void updateExistingNodes() {
+        String url = UriComponentsBuilder.newInstance()
+                .scheme("http").host(nodesInfo.getLoadBalancer())
+                .path("/node/map").toUriString();
+        RequestEntity<Void> request = RequestEntity.get(url)
+                .accept(MediaType.APPLICATION_JSON).build();
+        ParameterizedTypeReference<HashMap<Integer, Integer>> responseType = new ParameterizedTypeReference<HashMap<Integer, Integer>>() {
+        };
+        Map<Integer, Integer> nodeMap = restTemplate.exchange(request, responseType).getBody();
+        nodesInfo.setNodeMap(nodeMap.entrySet().stream().collect(Collectors.toMap(e -> e.getKey(), e -> "localhost:" + e.getValue())));
+    }
+
+    private void addSelf() {
+        String url = UriComponentsBuilder.newInstance()
+                .scheme("http").host(nodesInfo.getLoadBalancer())
+                .path(LB_ADD_NODE_PATH).buildAndExpand(nodesInfo.getSelfPort()).toUriString();
+        ResponseEntity<Integer> selfNode = restTemplate.getForEntity(url, Integer.class);
+        nodesInfo.setSelf(selfNode.getBody());
     }
 
 }
